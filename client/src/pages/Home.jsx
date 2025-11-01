8dbb31b75fadcf5d3d031c73f3f7a3a5b1a3472d
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2025-11-01 20:08:00 +0600

    Speciality part done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../components/Header'
+import Speciality from '../components/Speciality'
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Speciality />
     </div>
   )
 }
